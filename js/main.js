d293a219388d5bfdb49b36bd9d7fdaf7199c7811
elenabatsog <e.batsogianni@acg.edu>
2026-06-14 13:36:51 +0300

    Added quote function in the main.js through API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 //This is Javascript file for the homepage
 
 console.log("DanceFit Studio loaded successfully!");
+
+async function loadQuote() {
+    try {
+        const response = await fetch("https://api.quotable.io/random");
+
+        const data = await response.json();
+        const quoteText = document.querySelector("#quoteText");
+        const quoteAuthor = document.querySelector("#quoteAuthor");
+
+        quoteText.textContent = `"${data.content}"`;
+        quoteAuthor.textContent = `- ${data.author}`;
+    }
+
+    catch (error) {
+
+        const quoteText = document.querySelector("#quoteText");
+        const quoteAuthor = document.querySelector("#quoteAuthor");
+
+        quoteText.textContent = '"Great dancers are not great because of their technique, they are great because of their passion"';
+
+        quoteAuthor.textContent = "- Martha Graham";
+    }
+}
+
+//When the page loads, the function/quote will run
+loadQuote();
