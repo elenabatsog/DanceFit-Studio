bd85ae70da40906226d70f591ddfcac7984a7670
elenabatsog <e.batsogianni@acg.edu>
2026-06-18 12:21:13 +0300

    Began submit form function

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+const contactForm = document.querySelector("#contactForm");
+const contactForm = document.querySelector("#contactName");
+const contactForm = document.querySelector("#contactEmail");
+const contactForm = document.querySelector("#contactSubject");
+const contactForm = document.querySelector("#contactMessage");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if 
+})
