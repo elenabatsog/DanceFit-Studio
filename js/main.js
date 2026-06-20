774ab414f6eebe62e9057a10d02137594423ea45
elenabatsog <e.batsogianni@acg.edu>
2026-06-20 22:02:46 +0300

    Added comments on main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
-//This is Javascript file for the homepage
+//This is the main Javascript file for the entire website
 
+//the system checks if the file is loaded
 console.log("DanceFit Studio loaded successfully!");
 
+//A function that loads a quote using API
 async function loadQuote() {
     try {
+
+        //the system requests data from API
         const response = await fetch("https://api.quotable.io/random");
 
+        //it converts the response into json format
         const data = await response.json();
         const quoteText = document.querySelector("#quoteText");
         const quoteAuthor = document.querySelector("#quoteAuthor");
 
+        //the quote and the author are visible in the website
         quoteText.textContent = `"${data.content}"`;
         quoteAuthor.textContent = `- ${data.author}`;
     }
 
     catch (error) {
 
+        //the system finds and takes the elements
         const quoteText = document.querySelector("#quoteText");
         const quoteAuthor = document.querySelector("#quoteAuthor");
 
+        //if the random quote is not loaded, then a backup quote will be displayed to users
         quoteText.textContent = '"Great dancers are not great because of their technique, they are great because of their passion"';
 
         quoteAuthor.textContent = "- Martha Graham";
     }
 }
 
-//When the page loads, the function/quote will run
+//When the page loads, the function/quote will run only at homepage
 if (document.querySelector("#quoteText") && document.querySelector("#quoteAuthor")) {
     loadQuote();
 }
@@ -34,7 +42,11 @@ if (document.querySelector("#quoteText") && document.querySelector("#quoteAuthor
 const darkModeButton = document.querySelector("#darkModeBtn");
 if (darkModeButton) {
     darkModeButton.addEventListener("click", function() {
+
+        //the class for dark mode can be added or deleted
         document.body.classList.toggle("dark-mode");
+
+        //the button is changed based on the theme the user selects
         if (document.body.classList.contains("dark-mode")) {
             darkModeButton.textContent = "Light Mode";
 
