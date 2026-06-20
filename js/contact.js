6d76a678a32d31427a906f0ab69176cd0615b0cb
elenabatsog <e.batsogianni@acg.edu>
2026-06-20 20:25:37 +0300

    Added contact form validation

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,20 @@
 const contactForm = document.querySelector("#contactForm");
-const contactForm = document.querySelector("#contactName");
 const contactForm = document.querySelector("#contactEmail");
 const contactForm = document.querySelector("#contactSubject");
 const contactForm = document.querySelector("#contactMessage");
+const contactResult = document.querySelector("#contactResult");
 
 contactForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    if 
-})
+    const contactName = document.querySelector("#contactName").value;
+    if (contactName.length < 2) {
+        contactResult.textContent = "Please enter a valid name.";
+        return;
+    }
+
+    contactResult.textContent = "Your message has been submitted successfully.";
+    contactForm.requestFullscreen();
+});
+
+//Contact form validation
